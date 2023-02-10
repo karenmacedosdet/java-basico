a5925040b2ca222d2a482aed41bb5b84d0330f0b
kdevmacedo <kdevmacedo@gmail.com>
2023-02-10 00:48:33 -0300

    Atualiza Aula 11

--- src/com/loiane/cursojava/aula10/Variaveis.java
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -2,4 +2,18 @@ package com.loiane.cursojava.aula10;
 
 public class Variaveis {
 
+    public static void main(String[] args) {
+
+        int idade = 20;
+        String nome = "Loiane";
+        String nomeDoMeuCachorro = "Totó";
+        String ano2014 = "2014";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("Nome do meu cachorro = " + nomeDoMeuCachorro);
+        System.out.println("Ano = " + ano2014);
+
+    }
+
 }
---
+++ src/com/loiane/cursojava/aula11/VariaveisChar.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisChar {
+
+    public static void main(String[] args) {
+
+        char o = 'o';
+        char i = 'i';
+        char interrogacao =  0x003F;
+
+        System.out.println("" + o + i + interrogacao);
+
+    }
+
+}
